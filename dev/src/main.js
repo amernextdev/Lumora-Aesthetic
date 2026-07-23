5d58e71524bd6990e35d153277618f873e90168c
amernextdev <amernext.dev@gmail.com>
2026-07-23 08:28:59 +0300

    completed style.css

--- dev/src/main.js
+++ dev/src/main.js
@@ -1,10 +1,15 @@
 // /src/main.js
 
+// ====================
+//  - Imports -
+// ====================
+
 //  -- Styles --
 import './style.css'
 
 //  -- JavaScript --
 
 // services
+import '/src/services/js/reveal.js';
 
 // components
